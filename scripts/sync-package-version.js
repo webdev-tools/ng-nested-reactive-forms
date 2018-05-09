82a63d327b59c87812258cc064b746e9b1393da4
Carlos Gomes <carlos.gomes@thomsonreuters.com>
2018-05-08 22:17:45 -0300

    Added step to sync the main package version with the lib

---
+++ scripts/sync-package-version.js
@@ -0,0 +1,29 @@
+/*
+ * Sync the lib version with the main package.json file
+ *
+ */
+const path = require('path');
+const { writeFileSync } = require('fs');
+
+const rootFolder = path.resolve(__dirname, '../');
+const mainPackageJsonPath = path.join(rootFolder, 'package.json');
+const libPackageJsonPath = path.join(rootFolder, 'src/ng-nrforms/package.json');
+
+const mainPackageJson = require(mainPackageJsonPath);
+
+const libPackageJson = require(libPackageJsonPath);
+
+libPackageJson.version = mainPackageJson.version;
+
+
+const libPackageJsonContent = JSON.stringify(libPackageJson, null, 2) + '\n';
+
+try {
+  writeFileSync(libPackageJsonPath, libPackageJsonContent);
+
+  console.log('Sync Lib version success.');
+  console.log(`Package version: ${mainPackageJson.version}`);
+} catch (e) {
+  console.error(e);
+  process.exitCode = 5;
+}
